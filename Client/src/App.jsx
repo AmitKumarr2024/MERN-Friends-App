97f88bd86e3bf0210bca643ec79130df71bd0a31
Amit kumar <codes.amitkumar@gmail.com>
2025-01-04 00:00:06 +0530

    all file set commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
- 
-
   return (
     <>
-     <h1>Hello Amit</h1>
+      <h1 className="poppins-regular">Hello Amit</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
