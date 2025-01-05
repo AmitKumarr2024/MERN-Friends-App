4dc58156076caa31b0587f61757d0545e4ffa9ee
Amit kumar <codes.amitkumar@gmail.com>
2025-01-06 00:08:51 +0530

    Merged changes from remote main branch-7

--- Backend/index.js
+++ Backend/index.js
@@ -1,42 +1,42 @@
-import dotenv from "dotenv";
-dotenv.config(); // Call dotenv config at the top
+import path from 'path';
+dotenv.config();
 import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 import mongodb from "./Config/MongoDb.js";
-import UserRegister from "./Modules/User/User.routes.js";
-import FriendRequest from './Modules/friends/Friends.routes.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import UserRegister from "./Modules/User/User.routes.js"
+import FriendRequest from './Modules/friends/Friends.routes.js'
 
-// Equivalent to __dirname in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const port = process.env.PORT || 8006;
-
 app.use(express.json());
 app.use(cookieParser());
+
+const __dirname = path.resolve();
+
 
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "DELETE", "PATCH"],
+    methods: ["GET", "POST", "DELETE","PATCH"],
     credentials: true, // Allow credentials to be sent
   })
 );
 
 app.use(express.static(path.join(__dirname, "Client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Client", "dist", "index.html"));
-});
+
 
 app.use("/api/user/", UserRegister);
 app.use("/api/friends/", FriendRequest);
 
+
+
 app.listen(port, () => {
-  console.log(`Server started at port ${port}`);
+  console.log(`Server start at port ${port}`);
   mongodb();
+
 });
